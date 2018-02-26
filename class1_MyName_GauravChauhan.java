d8dade6d414a1a8bf67da386657037a8ce4f1cce
piyush gupta <piyushgupta752@gmail.com>
2018-02-26 22:41:53 +0530

    class 2 assignments

---
+++ class1_MyName_GauravChauhan.java
@@ -0,0 +1,26 @@
+
+public class class1_MyName_GauravChauhan {
+
+	//programs begins with call to main
+	
+		 public static void main(String args[])
+	     {
+			 //print command begins (Hi Everyone) 
+			 
+	
+	       System.out.println("Hi!! Everyone");
+	       
+	       //print command begins (I'm Gaurav Chauhan)
+	       
+	       System.out.println("I'm Gaurav Chauhan");
+	       
+	       //print commands begins (about statement.)
+	       
+	       System.out.println(" Currently I'm Pursuing My Second Year Bachelor Of Technology in Electronics and Communication Engineering. ");
+	       
+	       
+	     }
+}
+
+
+
